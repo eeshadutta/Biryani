ee168e69bb6550d77135f6017539ab2e9c0d5b38
eeshadutta <eeshadutta99@gmail.com>
2018-01-09 09:29:30 +0530

    Login page created

--- biryani/biryani/urls.py
+++ biryani/biryani/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import url, include
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^login/$', auth_views.login),
+    url(r'^logout/$', auth_views.logout),
+    url(r'^', include('calllogin.urls'))
 ]
